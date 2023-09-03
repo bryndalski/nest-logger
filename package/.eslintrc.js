4aad3a664f8a653a29368d4217ce0cece8f588a8
bryndalski <kubabryndal@gmail.com>
2023-09-03 18:58:09 +0200

    created and updated eslint rules fixed some major issues

---
+++ package/.eslintrc.js
@@ -0,0 +1,74 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'nestjs',
+    'jest',
+    'unused-imports',
+  ],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:nestjs/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-empty-function': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: [
+          'variable',
+          'function',
+          'classProperty',
+          'objectLiteralProperty',
+          'typeProperty',
+          'classMethod',
+        ],
+        format: ['strictCamelCase', 'PascalCase', 'UPPER_CASE'],
+      },
+      {
+        selector: 'variable',
+        types: ['boolean'],
+        prefix: ['is', 'should', 'has', 'can'],
+        format: ['camelCase', 'UPPER_CASE'],
+      },
+      {
+        selector: 'variable',
+        types: ['boolean'],
+        prefix: ['is', 'should', 'has', 'can'],
+        format: ['camelCase', 'UPPER_CASE'],
+      },
+      {
+        selector: ['enum', 'class'],
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'interface',
+        prefix: ['I'],
+        format: ['camelCase'],
+      },
+    ],
+    'require-await': 'off',
+    '@typescript-eslint/require-await': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': 'error',
+    '@typescript-eslint/consistent-type-exports': 'warn',
+  },
+};
--- package/src/Logger/Logger.core.module.ts
+++ package/src/Logger/Logger.core.module.ts
@@ -1,18 +1,18 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
 import { LoggerService } from './Logger.service';
-import { LoggerModuleRootOptions } from './types/LoggerModue.types';
 
 @Global()
 @Module({})
 export class LoggerModuleCore {
-  static rootLogger(options: LoggerModuleRootOptions): DynamicModule {
+  // options: LoggerModuleRootOptions
+  static rootLogger(): DynamicModule {
     return {
       module: LoggerModuleCore,
       providers: [LoggerService],
     };
   }
 
-  static forSubmodule(options: any): DynamicModule {
+  static forSubmodule(): DynamicModule {
     return {
       module: LoggerModuleCore,
       providers: [LoggerService],
