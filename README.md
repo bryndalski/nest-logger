b51740f32c8a9ad2df49a195f6b8f3bad554ffd1
bryndalski <kubabryndal@gmail.com>
2023-09-28 10:15:12 +0200

    Documentation (#8)
    * fixing unit test
    
    * created documentation

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# nest-boosted-logger
+<h1 style="text-align:center"> Nest logger </h1>
+
+[![Check unit tests](https://github.com/bryndalski/nest-logger/actions/workflows/unit_tests.yml/badge.svg?branch=master)](https://github.com/bryndalski/nest-logger/actions/workflows/unit_tests.yml)
+
+## Desctiption
+
+Nestjs logger userd for microserices. Provides connection to external loggin service and much more...
+
+## Installation
+
+```
+npm install @bryndalski/nest-logger
+```
+
+## Quick start
--- package/src/Logger/types/LoggerModue.types.ts
+++ package/src/Logger/types/LoggerModue.types.ts
@@ -2,24 +2,29 @@ import { LogLevel, LoggerService } from '@nestjs/common';
 
 export interface ILoggerModuleRootOptions extends Partial<LoggerService> {
   /**
-   * Production mode. Diable sending logs to api.
+   * If set to true logger will not print logs with debug level. It will also not send logs to external logging service.
+   * @default false
    */
   production?: boolean;
 
   /**
    * Api access token
+   * @default ''
+   *
    */
   apiToken: string;
 
   /**
    * Displayed log levels. By default all levels are displayed.
-   * @default ['log', 'error', 'warn', 'debug', 'verbose']
+   * @default ['log', 'error', 'warn', 'debug', 'verbose','fatal']
    * @example ['log', 'error']
    */
   logLevels?: LogLevel[];
 
   /**
    * Log levels send to api
+   * @default ['log', 'error', 'warn', 'fatal']
+   * @example ['log', 'error']
    */
-  persistLogLevels: LogLevel[];
+  apiLogLevels?: LogLevel[];
 }
